8f8c85424863f7335b677cf2f178a40ab7f3ecce
Arne Vangheel <94957070+ArneVangheel@users.noreply.github.com>
2022-12-28 15:23:25 +0100

    Update auth.py

--- api/auth.py
+++ api/auth.py
@@ -3,12 +3,14 @@ from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
 import crud
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
 
 # Configuratie
 SECRET_KEY = "454f5974f238422531030ba6889141e2a609b008d1f363fa8d39f7837175b393"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 pwd_context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
 
 
@@ -43,3 +45,21 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_customer(db: Session, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_customer_by_email(db, username)
+    if user is None:
+        raise credentials_exception
+    return user
